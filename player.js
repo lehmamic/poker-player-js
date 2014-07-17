223cd10948eaab230e9b37aa2b850ece18f29a07
lehmamic <leh@zuehlke.com>
2014-07-17 17:14:03 +0200

    Fix the always all in

--- player.js
+++ player.js
@@ -4,10 +4,29 @@ module.exports = {
   VERSION: "0.0.1",
 
   bet_request: function(game_state) {
-    return game_state.stack;
+      var me = getPlayer(game_state);
+      //for(var i = 0; i< player.hole_cards.length; i++) {
+
+      //}
+
+
+    return me.stack;
   },
 
   showdown: function(game_state) {
 
   }
 };
+
+function getPlayer (game_state) {
+    for (var i = 0; i< game_state.players.length; i++)
+    {
+        var player = hole_cards.players;
+        if(player.hole_cards.length > 0)
+        {
+            return player;
+        }
+    }
+
+    return null;
+}
