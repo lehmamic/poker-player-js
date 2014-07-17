4cae31520555c568add0898678fc242bb27abb8f
lehmamic <leh@zuehlke.com>
2014-07-17 17:32:58 +0200

    All in with high cards

--- player.js
+++ player.js
@@ -1,16 +1,19 @@
 
 module.exports = {
 
-  VERSION: "0.0.1",
+  VERSION: "0.0.2",
 
   bet_request: function(game_state) {
       var me = getPlayer(game_state);
-      //for(var i = 0; i< player.hole_cards.length; i++) {
 
-      //}
+      var hasHigh = hasHighCard(me.hole_cards);
 
-
-    return me.stack;
+      if(hasHigh && isBeforeFlop(game_state)) {
+          return me.stack;
+      }
+      else {
+          return 0;
+      }
   },
 
   showdown: function(game_state) {
@@ -21,7 +24,7 @@ module.exports = {
 function getPlayer (game_state) {
     for (var i = 0; i< game_state.players.length; i++)
     {
-        var player = hole_cards.players;
+        var player = hole_cards.players[i];
         if(player.hole_cards.length > 0)
         {
             return player;
@@ -29,4 +32,24 @@ function getPlayer (game_state) {
     }
 
     return null;
+}
+
+function hasHighCard(hole_cards) {
+    var isHigh = false;
+    for(var i = 0; i < hole_cards.length; i++) {
+        isHigh = isHighCard(hole_cards[i]);
+        if(isHigh) {
+            break;
+        }
+    }
+
+    return isHigh;
+}
+
+function isHighCard(card) {
+    return !IsNumeric(card.rank) ||  card.rank === '10';
+}
+
+function isBeforeFlop(game_state) {
+    return game_state.community_cards.length == 0;
 }
